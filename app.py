64d5e45b0b014fb0b315da5297d8ad40ea04fc08
Neha Koppikar <neha.koppikar@gmail.com>
2024-07-24 00:07:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,19 @@
 import os
 from flask import Flask, render_template, request, jsonify
-import google.generativeai as genai
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from collections import Counter
 import nltk
 import textstat
-from google.cloud import secretmanager
+from vertexai.generative_models import (
+    GenerationConfig,
+    GenerativeModel,
+    HarmBlockThreshold,
+    HarmCategory,
+    Part,
+)
 
-
-def access_secret_version(secret_id, version_id="latest"):
-    client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{os.environ['GOOGLE_CLOUD_PROJECT']}/secrets/{secret_id}/versions/{version_id}"
-    response = client.access_secret_version(request={"name": name})
-    return response.payload.data.decode("UTF-8")
-
-# Use this to get your API key
-api_key = access_secret_version("gemini-api-key")
-genai.configure(api_key=api_key)
 
 
 # Download necessary NLTK data
@@ -28,12 +23,54 @@ nltk.download('stopwords')
 
 app = Flask(__name__)
 
-# Configure Gemini API
-genai.configure(api_key="api_key")
-model = genai.GenerativeModel('gemini-pro')
+# Initialize Vertex AI
+#aiplatform.init(project=os.environ['GOOGLE_CLOUD_PROJECT'], location='us-central1')
 
 def generate_content(prompt):
-    response = model.generate_content(prompt)
+    # Get the Gemini model
+    #model = aiplatform.GenerativeModel("gemini-1.0-pro")
+
+    # Load Gemini 1.5 Pro Model
+    MODEL_ID = "gemini-1.5-pro-001"
+    model = GenerativeModel(MODEL_ID)
+
+    # Load a example model with system istructions
+    example_model = GenerativeModel(
+        MODEL_ID,
+        system_instruction=[
+            "You are a helpful text generator.",
+            "Your mission is help complete paragraphs.",
+        ],
+    )
+
+    # Set model parameters
+    generation_config = GenerationConfig(
+        temperature=0.9,
+        top_p=1.0,
+        top_k=32,
+        candidate_count=1,
+        max_output_tokens=8192,
+    )
+
+    # Set safety settings
+    safety_settings = {
+        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+        HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    }
+
+    # Set contents to send to the model
+    contents = [prompt]
+
+    # Prompt the model to generate content
+    response = example_model.generate_content(
+        contents,
+        generation_config=generation_config,
+        safety_settings=safety_settings,
+    )
+
+
     return response.text
 
 def analyze_content(text):
@@ -66,5 +103,8 @@ def index():
         return render_template('result.html', content=generated_content, analysis=analysis)
     return render_template('index.html')
 
+port = int(os.environ.get('PORT', 8080))
+#app.run(host='0.0.0.0', port=port)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=port)
